680a4dd1dd6cbda71f5ee97fd1b9f455db2a873c
FarhadJaan <FarhadJaan501@gmail.com>
2022-11-08 23:35:33 -0800

    thi is new commit

---
+++ src/porJectJin/JinClass.java
@@ -0,0 +1,21 @@
+package porJectJin;
+
+public class JinClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String name = "school is good ";
+		
+		int num = 120; 
+		
+		int numb = 40;
+		
+		if(num > numb ) {
+			System.out.println("this statement is true ");
+		}else 
+			System.out.println("this is statement is wrong");
+
+	}
+
+}
